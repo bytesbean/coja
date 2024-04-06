eb0a7e666760dd2789b18da50d8af7ca3d4321e4
dugm <19911108>
2024-04-06 16:31:10 +0800

    init

---
+++ coja-boot/coja-boot-starters/coja-boot-starter-web/src/main/java/org/bytesbean/coja/boot/web/api/ResultCode.java
@@ -0,0 +1,97 @@
+
+package org.bytesbean.coja.boot.web.api;
+
+
+import jakarta.servlet.http.HttpServletResponse;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 业务代码枚举
+ *
+ */
+@Getter
+@AllArgsConstructor
+public enum ResultCode {
+
+	/**
+	 * 操作成功
+	 */
+	SUCCESS(HttpServletResponse.SC_OK, "操作成功"),
+
+	/**
+	 * 业务异常
+	 */
+	FAILURE(HttpServletResponse.SC_BAD_REQUEST, "业务异常"),
+
+	/**
+	 * 请求未授权
+	 */
+	UN_AUTHORIZED(HttpServletResponse.SC_UNAUTHORIZED, "请求未授权"),
+
+	/**
+	 * 404 没找到请求
+	 */
+	NOT_FOUND(HttpServletResponse.SC_NOT_FOUND, "404 没找到请求"),
+
+	/**
+	 * 消息不能读取
+	 */
+	MSG_NOT_READABLE(HttpServletResponse.SC_BAD_REQUEST, "消息不能读取"),
+
+	/**
+	 * 不支持当前请求方法
+	 */
+	METHOD_NOT_SUPPORTED(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "不支持当前请求方法"),
+
+	/**
+	 * 不支持当前媒体类型
+	 */
+	MEDIA_TYPE_NOT_SUPPORTED(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "不支持当前媒体类型"),
+
+	/**
+	 * 请求被拒绝
+	 */
+	REQ_REJECT(HttpServletResponse.SC_FORBIDDEN, "请求被拒绝"),
+
+	/**
+	 * 服务器异常
+	 */
+	INTERNAL_SERVER_ERROR(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "服务器异常"),
+
+	/**
+	 * 缺少必要的请求参数
+	 */
+	PARAM_MISS(HttpServletResponse.SC_BAD_REQUEST, "缺少必要的请求参数"),
+
+	/**
+	 * 请求参数类型错误
+	 */
+	PARAM_TYPE_ERROR(HttpServletResponse.SC_BAD_REQUEST, "请求参数类型错误"),
+
+	/**
+	 * 请求参数绑定错误
+	 */
+	PARAM_BIND_ERROR(HttpServletResponse.SC_BAD_REQUEST, "请求参数绑定错误"),
+
+	/**
+	 * 参数校验失败
+	 */
+	PARAM_VALID_ERROR(HttpServletResponse.SC_BAD_REQUEST, "参数校验失败"),
+
+	/**
+	 * 限流
+	 */
+	LIMIT_REQUEST(HttpStatus.TOO_MANY_REQUESTS.value(), "超出请求频率限制"),
+	;
+
+	/**
+	 * code编码
+	 */
+	final int code;
+	/**
+	 * 中文信息描述
+	 */
+	final String message;
+
+}
